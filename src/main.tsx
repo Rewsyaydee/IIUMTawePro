b0046fb0df0fc23bb8989887e6226ec0ab600b8a
PuteraSedi <133309207+Rewsyaydee@users.noreply.github.com>
2026-07-27 10:26:47 +0800

    feat: add Vercel Speed Insights and Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import { MockDataProvider } from "./state/MockDataContext";
 import { MockUserProvider } from "./state/MockUserContext";
@@ -24,5 +26,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </MockDataProvider>
       </MockUserProvider>
     </BrowserRouter>
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
